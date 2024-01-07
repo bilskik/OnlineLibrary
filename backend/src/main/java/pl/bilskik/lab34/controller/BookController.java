ba1161ca4b3db94a3d4793fd39b14e32c836269b
bilskik <kbilski77@gmail.com>
2024-01-07 18:10:01 +0100

    final cmt

--- backend/src/main/java/pl/bilskik/lab34/controller/BookController.java
+++ backend/src/main/java/pl/bilskik/lab34/controller/BookController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import pl.bilskik.lab34.data.BookDTO;
 import pl.bilskik.lab34.data.ResponseMessage;
@@ -30,10 +31,12 @@ public class BookController {
     public  ResponseEntity<List<Book>> getAllBooks() {
         return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
     }
+
     @PostMapping
-    public  ResponseEntity<ResponseMessage> saveBook(@RequestBody @Valid BookDTO book) {
-        return new ResponseEntity<>(new ResponseMessage(bookService.saveBook(book)), HttpStatus.CREATED);
+    public  ResponseEntity<ResponseMessage> saveBook(@RequestBody @Valid BookDTO bookDTO) {
+        return new ResponseEntity<>(new ResponseMessage(bookService.saveBook(bookDTO)), HttpStatus.CREATED);
     }
+
     @PutMapping
     public  ResponseEntity<ResponseMessage> updateBook(@RequestBody @Valid Book book) {
         return new ResponseEntity<>(new ResponseMessage(bookService.updateBook(book)), HttpStatus.OK);
